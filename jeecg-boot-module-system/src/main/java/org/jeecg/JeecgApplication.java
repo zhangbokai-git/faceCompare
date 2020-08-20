075d72fc438e7c5e70cee805a003a8bca0db0961
zbk <>
2020-08-20 17:05:39 +0800

    数据库表监听canal

--- jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/JeecgApplication.java
@@ -1,5 +1,11 @@
 package org.jeecg;
 
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.CanalEntry.*;
+import com.alibaba.otter.canal.protocol.Message;
+import com.alibaba.otter.canal.protocol.exception.CanalClientException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.scan.StandardJarScanner;
@@ -16,10 +22,8 @@ import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.List;
 
 @Slf4j
 @EnableSwagger2
@@ -36,6 +40,49 @@ public class JeecgApplication {
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = env.getProperty("server.servlet.context-path");
+        // 创建链接
+        CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress("47.104.96.151",
+                11111), "example", "", "");
+        int batchSize = 1000;//获取指定数量的数据
+        int emptyCount = 0;//空数据数量
+        try {
+            connector.connect();
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            int totalEmptyCount = 120;//容忍空数据的数量
+            //while (emptyCount < totalEmptyCount) {
+                Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    emptyCount++;
+                    System.out.println("empty count : " + emptyCount);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                } else {
+                    emptyCount = 0;
+                    // System.out.printf("message[batchId=%s,size=%s] \n", batchId, size);
+                    printEntry(message.getEntries());
+                }
+
+                connector.ack(batchId); // 提交确认
+                // connector.rollback(batchId); // 处理失败, 回滚数据
+            //}
+
+            System.out.println("empty too many times, exit");
+        } catch (Exception e){
+            if(e instanceof CanalClientException){
+                CanalClientException clientException = (CanalClientException)e;
+                if(clientException.getCause() instanceof IOException || clientException.getCause() instanceof ConnectException){
+                    log.error("与canal服务器连接异常。。。。",e);
+                }
+            }
+            e.printStackTrace();
+        }finally {
+            connector.disconnect();
+        }
         try {
             ServerSocket serverSocket = new ServerSocket(10068);//创建绑定到特定端口的服务器Socket。
             Socket socket = null;//需要接收的客户端Socket
@@ -53,6 +100,8 @@ public class JeecgApplication {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application Jeecg-Boot is running! Access URLs:\n\t" +
@@ -75,5 +124,46 @@ public class JeecgApplication {
                 ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
             }
         };
+    }
+
+    private static void printEntry(List<Entry> entrys) {
+        for (Entry entry : entrys) {
+            if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN || entry.getEntryType() == EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            RowChange rowChage = null;
+            try {
+                rowChage = RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+
+            EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================> binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
+                    eventType));
+
+            for (RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------> before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------> after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<Column> columns) {
+        for (Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+        }
     }
 }
